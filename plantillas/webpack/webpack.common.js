97a4eb2c2d52837834a6f447ef25b68e787d48f3
Juan Camilo González <info@juancgonzalez.com>
2020-12-23 22:12:34 -0500

    ⬆️  ./ ruta pública + actualizar dependencias

--- plantillas/webpack/webpack.common.js
+++ plantillas/webpack/webpack.common.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: './src/programa.js',
   output: {
     path: path.resolve(__dirname, `./www`),
-    publicPath: '/',
+    publicPath: './',
     filename: '[name].[fullhash].js',
     chunkFilename: '[name].[fullhash].js',
   },
