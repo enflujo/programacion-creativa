5b75bcd4d90b84d0a50132b0e7de0ca03dd9bb47
3st3r30sc0p10 <h.idarraga@uniandes.edu.co>
2021-01-10 17:26:30 -0500

    cambios ejercicio lienzo hugo

--- ejercicios/lienzo-y-datos/3st3r30sc0p10/webpack/src/programa.js
+++ ejercicios/lienzo-y-datos/3st3r30sc0p10/webpack/src/programa.js
@@ -1,23 +1,22 @@
 import './scss/estilos.scss';
 
-console.log('Webpack');
 
 const canvas = document.getElementById("imgCanvas");
-const ctx = canvas.getContext("2d");
+const ctx = canvas.getContext('2d');
 
 function actualizar() {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
+    ctx.strokeStyle = '#990000'
+    ctx.lineWidth = 15;
+    
 }
   
 canvas.onmousemove = (evento) => {
     let posx = evento.clientX;
     let posy = evento.clientY;
-    console.log(posx, posy);
     ctx.lineTo(posx, posy);
     ctx.stroke();
-    ctx.strokeStyle = '#990000'
-    ctx.lineWidth = 15;
 };
 
 window.onresize = actualizar;
