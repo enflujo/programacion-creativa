0496b7df6388546991d5e5f29ae8013bc7264b6d
Jeanniffer Pimentel <jeannifferp@gmail.com>
2021-01-12 12:24:30 -0400

    Borré package.json

--- ejercicios/lienzo-y-datos/jeanniffer/index.js
+++ ejercicios/lienzo-y-datos/jeanniffer/index.js
@@ -20,9 +20,21 @@ miLienzo.onmousemove = (evento) => {
   const x = evento.clientX;
   const y = evento.clientY;
 
+// Juan Camilo González on Jan 11, 2021 at 12:17 PM
+// @jeanniffer: Ahora entramos en optimización de a poco, esta definición siempre
+// es la misma, es decir, cada que entramos al evento se define exactamente de la
+// misma forma, entonces puede estar por fuera del evento y sólo definirse cuando
+// es necesario. El punto sería luego de definir el `width` y el `height` de
+// `miLienzo` ya que cuando cambiamos el tamaño del lienzo se reinicia el contexto
+// del canvas. (Acabo de responder la pregunta que te hice en otro comentario, pero
+// intenta entender este concepto).
+// 
+// Juan Camilo González on Jan 11, 2021 at 12:23 PM
+// Adicionalmente, por rigor con el estilo, siempre usa el mismo estilo de comillas
+// en JS. Acá los veo `" "` y en otros lados `' '`.
   ctx.beginPath();
   ctx.arc(x, y, 10, 0, 2 * Math.PI, false);
-  ctx.fillStyle = "#96fffc"
+  ctx.fillStyle = '#96fffc';
   ctx.fill();
 
   /* Rectangulo
@@ -30,8 +42,6 @@ miLienzo.onmousemove = (evento) => {
   ctx.rect(x-25, y-25, 50, 50);
   ctx.stroke();
 */
-
-  console.log(x);
 };
 
 function actualizar() {
