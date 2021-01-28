61e7a2eceb662b2c713ef40dd2eb11c76a782dae
3st3r30sc0p10 <h.idarraga@uniandes.edu.co>
2021-01-27 23:38:13 -0500

    Nueva versión del robo. Más adelantos, pero aún no está listo

--- ejercicios/robo/3st3r30sc0p10/src/programa.js
+++ ejercicios/robo/3st3r30sc0p10/src/programa.js
@@ -3,6 +3,7 @@ import './scss/estilos.scss';
 const canvas = document.getElementById('lienzo');
 const ctx = canvas.getContext('2d');
 const p = [];
+const raton = { x: 0, y: 0 };
 
 function resize() {
     canvas.width = window.innerWidth;
@@ -12,37 +13,33 @@ function resize() {
 function setup() {
     ctx.fillStyle = '#000';
     ctx.fillRect(0, 0, canvas.width, canvas.height);
-    if (canvas.onmousedown) {
+    canvas.onmousedown = () => {
         draw();
         for (let i = 0; i < 200; i++) {
             p.push(new particle());
         }
-    } 
+    }    
+}
+canvas.onmousemove = (e) => {
+    raton.x = e.clientX;
+    raton.y = e.clientY;
 }
 
 function draw() {
     fade();
     purge();
     for (let i = 0; i < p.length; i++){
-        p.indexOf(i).step();
-        p.indexOf(i).display();
+        p[i].step();
+        p[i].display();
     }
 }
-// function mousePressed() {
-//     if (canvas.onmousedown) {
-//         draw();
-//         for (let i = 0; i < 200; i++) {
-//             p.push(new particle());
-//         }
-//     } 
-// }
-//
 
 function fade() {
     ctx.beginPath();
     ctx.fillStyle = 'rgba(0, 0, 0, 0.25)'
     ctx.rect(0, 0, canvas.width, canvas.height);
 }
+
 function getDistance(x1, y1, x2, y2) { 
 	var xDiff = x1 - x2; 
 	var yDiff = y1 - y2; 
@@ -63,34 +60,37 @@ function purge() {
 
 class particle {
     constructor(x, px, y, py, dir, mag, xv, yv, size, friction, gravity) {
-        this.x = parseFloat(mouseX);
-        this.px = parseFloat(x);
-        this.y = parseFloat(mouseY);
-        this.py = parseFloat(y);
-        this.dir = parseFloat(Math.random(0, TWO_PI));
-        this.mag = parseFloat(5 * (((width / 1920) * Math.random(0, 5)) + 0.1));
-        this.xv = parseFloat(mag * Math.cos(dir));
-        this.yv = parseFloat(mag * Math.sin(dir));
-        this.size = parseFloat(Math.random(1, 10));
-        this.friction = parseFloat(0.5);
-        this.gravity = parseFloat(0.2)
+        this.x = raton.x;
+        this.px = raton.x;
+        this.y = raton.y;
+        this.py = raton.y;
+        this.dir = Math.random(0, 2 * Math.PI);
+        this.mag = 5 * (((canvas.width / 1920) * aleatorio(0, 5)) + 0.1);
+        this.xv = this.mag * Math.cos(this.dir);
+        this.yv = this.mag * Math.sin(this.dir);
+        this.size = aleatorio(1, 10);
+        this.friction = 0.5;
+        this.gravity = 0.2;
+        function aleatorio(min, max) {
+            return Math.random() * (max - min) + min;
+        }
     }
     display() {
         ctx.beginPath();
         ctx.moveTo(this.px, this.py);
         ctx.lineTo(this.x, this.y);
-        ctx.lineWidth(this.size);
+        ctx.lineWidth = this.size;
         ctx.strokeStyle = '#fff';
         ctx.stroke();
     }
-    setup() {
+    step() {
         this.px = this.x;
         this.py = this.y;
         this.x += this.xv;
         this.y += this.yv;
         this.yv = this.gravity;
         //derecha
-        if (this.x > width - (this.size / 2)) {
+        if (this.x > canvas.width - (this.size / 2)) {
             this.x -= this.xv;
             this.xv = -this.friction * Math.abs(this.xv);
             this.yv *= this.friction;
