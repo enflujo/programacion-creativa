9721880dff471c302cc3a0ee724c3a7d93abf841
Jeanniffer Pimentel <jeannifferp@gmail.com>
2021-01-31 15:45:02 -0400

    Correcciones RAF

--- ejercicios/robo/jeanniffer/src/programa.js
+++ ejercicios/robo/jeanniffer/src/programa.js
@@ -11,24 +11,23 @@ function actualizar() {
   ctx.translate(window.innerWidth / 2, window.innerHeight / 2);
 }
 
-function animacion() {
-  requestAnimationFrame(function (timestamp) {
-    ctx.fillStyle = '#FFFFFF';
+function animacion(timestamp) {
+  ctx.fillStyle = '#FFFFFF';
+  ctx.beginPath();
+  ctx.rect(0 - 300, 0 - 300, 600, 600);
+  ctx.fill();
+  for (let i = 0; i < 500; i++) {
+    const frecuencia = 0.000005;
+    const circulo = 200 + 50 * Math.sin(timestamp * frecuencia * i);
+    const color = mapear(circulo, 150, 250, 255, 60);
+    const radio = mapear(circulo, 150, 250, 5, 2);
+    ctx.fillStyle = `rgba(${color}, 0, 74)`;
     ctx.beginPath();
-    ctx.rect(0 - 300, 0 - 300, 600, 600);
+    ctx.arc(circulo * Math.cos(i), circulo * Math.sin(i), radio, 0, 2 * Math.PI);
     ctx.fill();
-    for (let i = 0; i < 500; i++) {
-      const frecuencia = 0.000005;
-      const circulo = 200 + 50 * Math.sin(timestamp * frecuencia * i);
-      const color = mapear(circulo, 150, 250, 255, 60);
-      const radio = mapear(circulo, 150, 250, 5, 2);
-      ctx.fillStyle = `rgba(${color}, 0, 74)`;
-      ctx.beginPath();
-      ctx.arc(circulo * Math.cos(i), circulo * Math.sin(i), radio, 0, 2 * Math.PI);
-      ctx.fill();
-      rotacion = rotacion + 0.00005;
-    }
-  });
+    rotacion = rotacion + 0.00005;
+  }
+
   requestAnimationFrame(animacion);
 }
 
@@ -38,4 +37,4 @@ function mapear(valor, color1, color2, opacidad1, opacidad2) {
 
 window.onresize = actualizar;
 actualizar();
-animacion();
+requestAnimationFrame(animacion);
