63536f3f4c427009c98327e76cc8b3b0ff9d543e
3st3r30sc0p10 <h.idarraga@uniandes.edu.co>
2021-03-12 17:35:30 -0500

    Cambié lo que inicialmente iba a hacer, pero me gustó así. ¿Cómo te parece que quedó?

--- ejercicios/clima/3st3r30sc0p10/src/programa.js
+++ ejercicios/clima/3st3r30sc0p10/src/programa.js
@@ -7,15 +7,27 @@ window.addEventListener('load', () => {
 const name = document.querySelector('.name');
 const desc = document.querySelector('.desc');
 const temperatura = document.querySelector('.temperatura');
-  const icono = document.querySelector('.icono');
+const icono = document.querySelector('.icono');
   const mainWeather = document.querySelector('.mainWeather');
-// const inicio = document.getElementById('inicio');
-// const intro = document.getElementById('pg1');
-// const pg2Div = document.getElementById('pg2');
+  const valorHumedad = document.querySelector('.valorHumedad');
+  const valorFeels_like = document.querySelector('.valorFeels_like')
+  const valorPresion = document.querySelector('.valorPresion')
+  const canvasIcon2 = document.querySelector('.canvasIcon2')
 
-// inicio.addEventListener('click', () => {
-//   intro.classList.add('hidden');
-//   pg2Div.classList.remove('hidden');
+// class Nube {
+//   constructor(temp, feels_like, humidity, pressure, temp_max, temp_min) {
+//     this.temp = temp;
+//     this.feels_like = feels_like;
+//     this.humidity = humidity;
+//     this.pressure = pressure;
+//     this.temp_max = temp_max;
+//     this.temp_min = temp_min;
+//   }
+//   pintar(temp) {
+//     ctx3.fillStyle = 'rgb(3, 196, 252)';
+//     ctx3.fill();
+//   }
+// }
 
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(position => {
@@ -24,25 +36,66 @@ const temperatura = document.querySelector('.temperatura');
       long = position.coords.longitude;
       lat = position.coords.latitude;
       let key = '44f3307752745b5ea4641ada8dc2e312';
-      fetch('https://api.openweathermap.org/data/2.5/weather?' + 'lat=' + `${lat}` + '&lon=' + `${long}` + '&appid=' + key)
+      fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${key}`)
       .then(response => response.json())
         .then(data => {
           const { temp, feels_like, humidity, pressure, temp_max, temp_min } = data.main;
-          const { description, icon, id, main} = data.weather[0];
+          const { description, icon, id, main } = data.weather[0];
           let nameValue = data['name'];
           let valorTemperatura = temp;
           let descValue = description;
           let weatherMain = main;
-          let iconSrc = "http://openweathermap.org/img/w/" + `${icon}` + ".png";
+          let iconSrc = `http://openweathermap.org/img/w/${icon}.png`;
           let celcius = Math.round(parseFloat(valorTemperatura) - 273.15);
           let img = new Image();
+          let humedad = humidity;
+          let Feels_Like = feels_like;
+          let celcius_fells_like = Math.round(parseFloat(Feels_Like) - 273.15);
+          let presion = pressure;
   
           name.innerHTML = nameValue;
           temperatura.innerHTML = celcius + '&deg;C';
           desc.innerHTML = descValue;
-          icono.innerHTML = '<img src=' + iconSrc + '>';
+          icono.innerHTML = `<img src=${iconSrc}>`;
           mainWeather.innerHTML = weatherMain;
+          valorHumedad.innerHTML = humedad;
+          valorFeels_like.innerHTML = celcius_fells_like;
+          valorPresion.innerHTML = presion;
+          canvasIcon2.innerHTML = weatherMain;
           
+          if (humidity >= 75) {
+            valorHumedad.className = 'humedad_alta'
+          }
+          else if (humidity >= 50 && humidity < 75) {
+            valorHumedad.className = 'humedad_media_alta'
+          }
+          else if (humidity >= 25 && humidity < 50) {
+            valorHumedad.className = 'humedad_medio_baja'
+          }
+          else if (humidity >= 0 && humidity < 25) {
+            valorHumedad.className = 'humedad_baja'
+          }
+
+          if (celcius_fells_like >=25) {
+            valorFeels_like.className = 'fells_like_calor'
+          }
+          else if (celcius_fells_like >= 15 && celcius_fells_like < 25) {
+            valorFeels_like.className = 'fells_like_bien'
+          }
+          else if (celcius_fells_like >= 1 && celcius_fells_like < 15) {
+            valorFeels_like.className = 'fells_like_frio'
+          }
+
+          if (pressure >=1050) {
+            valorPresion.className = 'pressure_alta'
+          }
+          else if (pressure >= 1000 && feels_like < 1050) {
+            valorPresion.className = 'pressure_normal'
+          }
+          else if (pressure >= 800 && feels_like < 1000) {
+            valorPresion.className = 'pressure_baja'
+          }
+
           if (data.weather.length > 0) {
           const {main} = data.weather[0];
             if(main === 'Clouds') {
@@ -66,106 +119,42 @@ const temperatura = document.querySelector('.temperatura');
               canvasIcon2.className = 'thunderstormImage';
             }
           } 
-          // canvas.width = img.width*10;
-          // canvas.height = img.height * 10;
           img.src = iconSrc;
           img.onload = function () {
-            fill_canvas(img);  
             animate();
-        }
+          }
           
-        function animate() {
+          function animate() {
           img.src;
-          setInterval(fill_canvas(img), 100);
+          fill_canvas(img);
           requestAnimationFrame(animate);
          }
-
         function fill_canvas(img) {
           let canvas = document.getElementById('canvasIcon');
-          let canvas2 = document.getElementById('canvasIcon2');
-          let canvas3 = document.getElementById('canvasIcon3');
-          let canvas4 = document.getElementById('canvasIcon4');
-          let canvas5 = document.getElementById('canvasIcon5');
           let ctx = canvas.getContext('2d');
-          let ctx2 = canvas2.getContext('2d');
-          let ctx3 = canvas3.getContext('2d');
-          let ctx4 = canvas4.getContext('2d');
-          let ctx5 = canvas5.getContext('2d');
           ctx.fillStyle = 'rgba(0,0,0,0.4)';
-               ctx.strokeStyle = 'rgba(0,153,255,0.4)';
-               ctx.save();
-               ctx.translate(150,150); 
+          ctx.strokeStyle = 'rgba(0,153,255,0.4)';
+          ctx.save();
+          ctx.translate(150,150); 
                
-               let time = new Date(); 
-               ctx.rotate( (((celcius/5)*Math.PI)/6)*time.getSeconds() + ( ((celcius/5)*Math.PI)/6000)*time.getMilliseconds() );
-               ctx.translate(10,78.5);
-               ctx.drawImage(img,-3.5,-3.5);
+          let time = new Date(); 
+          ctx.rotate( (((celcius/5)*Math.PI)/6)*time.getSeconds() + ( ((celcius/5)*Math.PI)/6000)*time.getMilliseconds() );
+          ctx.translate(10,78.5);
+          ctx.drawImage(img,-3.5,-3.5);
           ctx.restore();
           
-          
-              
-               ctx2.drawImage(img,-3.5,-3.5);
             
-          ctx3.fillStyle = 'rgba(0,0,0,0.4)';
-               ctx3.strokeStyle = 'rgba(0,153,255,0.4)';
-               ctx3.save();
-               ctx3.translate(150,150); 
-               
-               let time3 = new Date(); 
-               ctx3.rotate( ((2*Math.PI)/6)*time3.getSeconds() + ( (2*Math.PI)/6000)*time3.getMilliseconds() );
-               ctx3.translate(0,28.5);
-               ctx3.drawImage(img,-3.5,-3.5);
-          ctx3.restore();
-          ctx4.fillStyle = 'rgba(0,0,0,0.4)';
-               ctx4.strokeStyle = 'rgba(0,153,255,0.4)';
-               ctx4.save();
-               ctx4.translate(150,150); 
-               
-               let time4 = new Date(); 
-               ctx4.rotate( ((2*Math.PI)/6)*time4.getSeconds() + ( (2*Math.PI)/6000)*time4.getMilliseconds() );
-               ctx4.translate(0,28.5);
-               ctx4.drawImage(img,-3.5,-3.5);
-          ctx4.restore();
-          ctx5.fillStyle = 'rgba(0,0,0,0.4)';
-               ctx5.strokeStyle = 'rgba(0,153,255,0.4)';
-               ctx5.save();
-               ctx5.translate(150,150); 
-               
-               let time5 = new Date(); 
-               ctx5.rotate( ((2*Math.PI)/6)*time5.getSeconds() + ( (2*Math.PI)/6000)*time5.getMilliseconds() );
-               ctx5.translate(0,28.5);
-               ctx5.drawImage(img,-3.5,-3.5);
-          ctx5.restore();
           
         }
-        console.log(data);
+          console.log(data);
     })
   
-    .catch(err => console.log(err))
+        .catch(err => console.log(err))
+      
     })
+    
   }
 });
 
 
 
-// button.addEventListener('click', function () {
-//   let key = '44f3307752745b5ea4641ada8dc2e312';
-//   fetch('https://api.openweathermap.org/data/2.5/weather?q=' + inputValue.value + '&appid=' + key)
-//   .then(response => response.json())
-//   .then(data => {
-//       let nameValue = data['name'];
-//       let tempValue = data['main']['temp'];
-//       let celcius = Math.round(parseFloat(tempValue)-273.15);
-//       let descValue = data['weather'][0]['description'];
-//       let otrosValue = data['main']['feels_like'];
-
-//       name.innerHTML = nameValue;
-//       temp.innerHTML = celcius + '&deg;C';
-//       desc.innerHTML = descValue;
-//       otros.innerHTML = otrosValue;
-
-//       console.log(data);
-//   })
-
-//   .catch(err => alert("El nombre de la ciudad es inexistente!"))
-// })
