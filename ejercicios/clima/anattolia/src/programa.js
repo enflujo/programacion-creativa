504d777ddfdcc12e418fbc5522b23111b8232849
anattolia <anattolia@gmail.com>
2021-03-25 20:21:39 -0700

    modificaciones clima anattolia

--- ejercicios/clima/anattolia/src/programa.js
+++ ejercicios/clima/anattolia/src/programa.js
@@ -7,6 +7,9 @@ import neblina from './assets/olgaluciahurtado_mist.jpeg';
 import niebla from './assets/fog_miguel_angel_rojas.jpeg';
 import rotas from './assets/lm_broken.jpeg';
 import pocas from './assets/lm_few.jpeg';
+import nieve from './assets/mh_nieve.jpg';
+import trueno from './assets/mh_trueno.jpeg';
+import haze from './assets/mh_haze.jpeg';
 
 const lienzo = document.getElementById('lienzo');
 const ctx = lienzo.getContext('2d');
@@ -32,108 +35,31 @@ function imprimirImg() {
   ctx.drawImage(img, 0, 0, lienzo.width, lienzo.height);
 }
 
-function imprimirCoor(r, g, b) {
+function imprimirCoor([r, g, b]) {
   ctx.fillStyle = `rgba(${r}, ${g}, ${b}, 0.9)`;
   ctx.font = `${lienzo.width / 75}px Monospace`;
   ctx.fillText(lat + ', ' + lon, lienzo.width / 30, lienzo.height / 15);
+  //console.log(r, g, b);
 }
 
-//Los valores de tamaños son inversamente proporcionales porque dividen el tamaño de pantalla
-//La posX no cambia pero la dejo para mantener la estructura
-function imprimirTxt(posX, posY, tFuente, r, g, b, p1 = '', p2 = '', p3 = '') {
+function imprimirTxt([posX, posY, tFuente, r, g, b], p1 = '', p2 = '') {
   const posH = lienzo.width / posX;
   const posV = lienzo.height / posY;
   ctx.font = `${lienzo.width / tFuente}px Monospace`;
   ctx.fillStyle = `rgba(${r}, ${g}, ${b}, 0.8)`;
   ctx.fillText(p1, posH, posV);
   ctx.fillText(p2, posH, posV + ctx.font[0] * 7);
-  ctx.fillText(p3, posH, posV + ctx.font[0] * 14);
 }
 
-function clear() {
-  img.src = despejado;
-  imprimirImg();
-  var pa1 = 'Transparentes los aires,';
-  var pa2 = 'transparentes.';
-  imprimirTxt(30, 1.5, 18, 20, 20, 20, pa1, pa2);
-  imprimirCoor(0, 0, 0);
-  creditos.innerHTML = '"Aeropuerto El Dorado y sus alrededores", Manuel H';
-  document.body.style.backgroundColor = 'rgba(10, 10, 10)';
-}
-
-function broken_clouds() {
-  img.src = rotas;
-  imprimirImg();
-  var pa1 = 'El temporal fue unánime';
-  var pa2 = 'y aborrecible a las miradas fue el mundo';
-  imprimirTxt(30, 1.5, 29, 250, 250, 250, pa1, pa2);
-  imprimirCoor(50, 50, 50);
-  creditos.innerHTML = '"Bomberos apagando un incendio", Leo Matiz';
-  document.body.style.backgroundColor = 'rgba(10, 10, 10)';
-}
-
-function scattered_clouds() {
-  img.src = dispersas;
-  imprimirImg();
-  var pa1 = 'La numerosa nube que se deshace en el poniente';
-  var pa2 = 'es nuestra imagen.';
-  imprimirTxt(30, 2, 30, 250, 250, 250, pa1, pa2);
-  imprimirCoor(70, 70, 70);
-  creditos.innerHTML = 'Foto: Leo Matiz';
-  document.body.style.backgroundColor = 'rgba(10, 10, 10)';
-}
-
-function overcast_clouds() {
-  img.src = over;
-  imprimirImg();
-  var pa1 = 'No habrá una sola cosa que no sea una nube.';
-  imprimirTxt(30, 1.6, 30, 25, 25, 25, pa1);
-  imprimirCoor(70, 70, 70);
-  creditos.innerHTML = '"La primera chispa del bogotazo 1948 II", Daniel Rodríguez';
-  document.body.style.backgroundColor = 'rgba(10, 10, 10)';
-}
-
-function few_clouds() {
-  img.src = pocas;
-  imprimirImg();
-  var pa1 = 'Hay soledad en el hogar sin bulla.';
-  imprimirTxt(30, 1.6, 30, 250, 250, 250, pa1);
-  imprimirCoor(20, 20, 20);
-  creditos.innerHTML = 'Foto: Leo Matiz';
-  document.body.style.backgroundColor = 'rgba(50, 40, 40)';
-}
-
-function rain() {
-  img.src = lluvia;
-  imprimirImg();
-  var pa1 = 'Quiero volver a tierras niñas;';
-  var pa2 = 'llévenme a un blando país de aguas.';
-  imprimirTxt(30, 2.3, 25, 25, 25, 25, pa1, pa2);
-  imprimirCoor(210, 210, 210);
-  creditos.innerHTML = '"Carro de Bomberos en la Av. Jiménez por las Lluvias", Leo Matiz';
-  document.body.style.backgroundColor = 'rgba(220, 223, 228)';
-}
-
-function mist() {
-  img.src = neblina;
-  imprimirImg();
-  var pa1 = 'Esta penumbra es lenta y no duele;';
-  var pa2 = 'fluye por un manso declive y se parece a la eternidad.';
-  imprimirTxt(30, 2.3, 35, 25, 25, 25, pa1, pa2);
-  imprimirCoor(100, 100, 120);
-  creditos.innerHTML = '"Evanescente 5", Olga Lucía Hurtado';
-  document.body.style.backgroundColor = 'rgba(10, 13, 18)';
-}
-
-function fog() {
-  img.src = niebla;
-  imprimirImg();
-  var pa1 = 'Verano, ya me voy. Y me dan pena';
-  var pa2 = 'las manitas sumisas de tus tardes.';
-  imprimirTxt(30, 1.7, 28, 255, 255, 255, pa1, pa2);
-  imprimirCoor(255, 250, 250);
-  creditos.innerHTML = '"Fanny (Serie Faenza)",  Miguel Angel Rojas';
-  document.body.style.backgroundColor = 'rgba(230, 230, 230)';
+function pintar(datos) {
+  img.src = datos.img;
+  img.onload = () => {
+    imprimirImg();
+    imprimirCoor(datos.color);
+    imprimirTxt(datos.fuente, datos.p1, datos.p2);
+    creditos.innerHTML = datos.creditos;
+    document.body.style.backgroundColor = datos.fondo;
+  };
 }
 
 function dibujar() {
@@ -147,27 +73,123 @@ function dibujar() {
 
   async function obtenerClima(lat, lon) {
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+    //const url = `https://api.openweathermap.org/data/2.5/weather?lat=4&lon=73&appid=${apiKey}`;
+
     const respuesta = await fetch(url);
     const datos = await respuesta.json();
 
     clima = datos.weather[0].description;
-    //console.log(clima);
+    console.log(clima);
     if (clima.includes('clear')) {
-      clear();
+      pintar({
+        img: despejado,
+        fuente: [30, 1.5, 18, 20, 20, 20],
+        p1: 'Transparentes los aires,',
+        p2: 'transparentes',
+        color: [0, 0, 0],
+        creditos: '"Aeropuerto El Dorado y sus alrededores", Manuel H',
+        fondo: 'rgba(10,10,10)',
+      });
     } else if (clima.includes('broken clouds')) {
-      broken_clouds();
+      pintar({
+        img: rotas,
+        fuente: [30, 1.5, 29, 250, 250, 250],
+        p1: 'El temporal fue unánime',
+        p2: 'y aborrecible a las miradas fue el mundo.',
+        color: [50, 50, 50],
+        creditos: '"Bomberos apagando un incendio", Leo Matiz',
+        fondo: 'rgba(10,10,10)',
+      });
     } else if (clima.includes('scattered clouds')) {
-      scattered_clouds();
+      pintar({
+        img: dispersas,
+        fuente: [30, 2, 30, 250, 250, 250],
+        p1: 'La numerosa nube que se deshace en el poniente',
+        p2: 'es nuestra imagen.',
+        color: [70, 70, 70],
+        creditos: 'Foto: Leo Matiz',
+        fondo: 'rgba(10,10,10)',
+      });
     } else if (clima.includes('overcast clouds')) {
-      overcast_clouds();
+      pintar({
+        img: over,
+        fuente: [30, 1.6, 30, 25, 25, 25],
+        p1: 'No habrá una sola cosa que no sea una nube.',
+        p2: '',
+        color: [70, 70, 70],
+        creditos: '"La primera chispa del bogotazo 1948 II", Daniel Rodríguez',
+        fondo: 'rgba(10,10,10)',
+      });
     } else if (clima.includes('few clouds')) {
-      few_clouds();
+      pintar({
+        img: pocas,
+        fuente: [30, 1.6, 30, 220, 220, 220],
+        p1: 'Hay soledad en el hogar sin bulla.',
+        p2: '',
+        color: [220, 220, 220],
+        creditos: 'Foto: Leo Matiz',
+        fondo: 'rgba(0, 0, 0)',
+      });
     } else if (clima.includes('rain')) {
-      rain();
+      pintar({
+        img: lluvia,
+        fuente: [30, 2.3, 25, 25, 25, 25],
+        p1: 'Quiero volver a tierras niñas;',
+        p2: 'llévenme a un blando país de aguas.',
+        color: [210, 210, 210],
+        creditos: '"Carro de Bomberos en la Av. Jiménez por las Lluvias", Leo Matiz',
+        fondo: 'rgba(220, 223, 228)',
+      });
     } else if (clima.includes('mist')) {
-      mist();
+      pintar({
+        img: neblina,
+        fuente: [30, 2.3, 35, 15, 15, 15],
+        p1: 'Esta penumbra es lenta y no duele;',
+        p2: 'fluye por un manso declive y se parece a la eternidad.',
+        color: [25, 25, 25],
+        creditos: '"Evanescente 5", Olga Lucía Hurtado',
+        fondo: 'rgba(10, 13, 18)',
+      });
     } else if (clima.includes('fog')) {
-      fog();
+      pintar({
+        img: niebla,
+        fuente: [30, 1.7, 28, 255, 255, 255],
+        p1: 'Verano, ya me voy. Y me dan pena',
+        p2: 'las manitas sumisas de tus tardes.',
+        color: [255, 250, 250],
+        creditos: '"Fanny (Serie Faenza)", Miguel Angel Rojas',
+        fondo: 'rgba(230, 230, 230)',
+      });
+    } else if (clima.includes('snow')) {
+      pintar({
+        img: nieve,
+        fuente: [30, 1.7, 28, 15, 15, 15],
+        p1: 'en esta hora fría, en que la tierra',
+        p2: 'trasciende a polvo humano y es tan triste',
+        color: [15, 15, 15],
+        creditos: 'Foto: Manuel H',
+        fondo: 'rgba(20, 20, 20)',
+      });
+    } else if (clima.includes('thundersorm')) {
+      pintar({
+        img: trueno,
+        fuente: [30, 1.5, 28, 255, 255, 255],
+        p1: 'las alas no se me ven, los oídos me retumban',
+        p2: '',
+        color: [220, 220, 220],
+        creditos: 'Foto: Manuel H',
+        fondo: 'rgba(20, 20, 20)',
+      });
+    } else if (clima.includes('haze')) {
+      pintar({
+        img: haze,
+        fuente: [30, 1.8, 30, 15, 15, 15],
+        p1: 'tiene un día cualquiera mayor aire en sus alas',
+        p2: '',
+        color: [25, 25, 25],
+        creditos: 'Foto: Manuel H',
+        fondo: 'rgba(250, 250, 250)',
+      });
     } else {
       ctx.fillText(clima, 100, 200);
     }
@@ -179,20 +201,19 @@ function dibujar() {
 
 //Cambiar los colores de la imagen al hacer click
 function click() {
-  let img = 1;
+  var estadoImg = 1;
   lienzo.onclick = () => {
     if (pixeles.length) {
-      if (img === 1) {
-        for (let i = 0; i < pixeles.length; i += 4) {
-          pixeles[i] = pixeles[i];
+      if (estadoImg === 1) {
+        for (var i = 0; i < pixeles.length; i += 4) {
           pixeles[i + 1] = pixeles[200];
           pixeles[i + 2] = pixeles[i + 3];
           document.body.style.backgroundColor = 'rgba(0, 0, 0)';
         }
-        img = 0;
+        estadoImg = 0;
       } else if (img === 0) {
         dibujar();
-        img = 1;
+        estadoImg = 1;
       }
     }
     //Poner de nuevo los datos en pantalla
